42a8056ff85c6192b2383f37377e1c10b90d48f0
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-10-01 08:54:34 +0200

    refactor(posts): HTML code slimmed in update and create CRUDs

--- resources/views/admin/posts/partials/input_form.blade.php
+++ resources/views/admin/posts/partials/input_form.blade.php
@@ -10,119 +10,109 @@
         @method('PUT')
     @endif
 
-    <div class="col-12">
-        <div class="position-relative pb-4">
-            <label for="input-title" class="form-label text-primary">
-                Title
-            </label>
+    <div class="col-12 position-relative pb-4">
+        <label for="input-title" class="form-label text-primary">
+            Title
+        </label>
 
-            <input type="text" class="form-control @error('title') is-invalid @enderror" id="input-title" name="title"
-                aria-errormessage="input-title-error" value="{!! old('title', $is_update_form ? $item_to_update->title : '') !!}" minlength="3" maxlength="150"
-                placeholder="Title..." required>
+        <input type="text" class="form-control @error('title') is-invalid @enderror" id="input-title" name="title"
+            aria-errormessage="input-title-error" value="{!! old('title', $is_update_form ? $item_to_update->title : '') !!}" minlength="3" maxlength="150"
+            placeholder="Title..." required>
 
-            @error('title')
-                <small id="input-title-error" class="invalid-feedback position-absolute bottom-0 start-0">
-                    {{ $message }}
-                </small>
-            @enderror
-        </div>
+        @error('title')
+            <small id="input-title-error" class="invalid-feedback position-absolute bottom-0 start-0">
+                {{ $message }}
+            </small>
+        @enderror
     </div>
 
-    <div class="col-12">
-        <div class="position-relative pb-4">
-            <label for="input-body" class="form-label text-primary">
-                Body
-            </label>
+    <div class="col-12 position-relative pb-4">
+        <label for="input-body" class="form-label text-primary">
+            Body
+        </label>
 
-            <textarea type="text" class="form-control @error('body') is-invalid @enderror" id="input-body" name="body"
-                aria-errormessage="input-body-error" rows="6" minlength="3" maxlength="65535" placeholder="Body..." required>{!! old('body', $is_update_form ? $item_to_update->body : '') !!}</textarea>
+        <textarea type="text" class="form-control @error('body') is-invalid @enderror" id="input-body" name="body"
+            aria-errormessage="input-body-error" rows="6" minlength="3" maxlength="65535" placeholder="Body..." required>{!! old('body', $is_update_form ? $item_to_update->body : '') !!}</textarea>
 
-            @error('body')
-                <small id="input-body-error" class="invalid-feedback position-absolute bottom-0 start-0">
-                    {{ $message }}
-                </small>
-            @enderror
-        </div>
+        @error('body')
+            <small id="input-body-error" class="invalid-feedback position-absolute bottom-0 start-0">
+                {{ $message }}
+            </small>
+        @enderror
     </div>
 
-    <div class="col-12">
-        <div class="position-relative pb-4">
-            <label for="input-archived-status" class="form-label text-primary">
-                Type
-            </label>
+    <div class="col-12 position-relative pb-4">
+        <label for="input-archived-status" class="form-label text-primary">
+            Type
+        </label>
 
-            <select class="form-control @error('post_type_id') is-invalid @enderror" id="input-archived-status"
-                name="post_type_id" aria-errormessage="input-archived-status-error">
-                <option value="" @selected(old('post_type_id', $is_update_form ? $item_to_update->post_type_id : null) == null)>
-                    None
+        <select class="form-control @error('post_type_id') is-invalid @enderror" id="input-archived-status"
+            name="post_type_id" aria-errormessage="input-archived-status-error">
+            <option value="" @selected(old('post_type_id', $is_update_form ? $item_to_update->post_type_id : null) == null)>
+                None
+            </option>
+
+            @foreach ($types as $type)
+                <option value="{!! $type->id !!}" @selected(old('post_type_id', $is_update_form ? $item_to_update->post_type_id : null) == $type->id)>
+                    {!! $type->name !!}
                 </option>
-
-                @foreach ($types as $type)
-                    <option value="{!! $type->id !!}" @selected(old('post_type_id', $is_update_form ? $item_to_update->post_type_id : null) == $type->id)>
-                        {!! $type->name !!}
-                    </option>
-                @endforeach
-            </select>
-            @error('post_type_id')
-                <small id="input-archived-status-error" class="invalid-feedback position-absolute bottom-0 start-0">
-                    {{ $message }}
-                </small>
-            @enderror
-        </div>
+            @endforeach
+        </select>
+        @error('post_type_id')
+            <small id="input-archived-status-error" class="invalid-feedback position-absolute bottom-0 start-0">
+                {{ $message }}
+            </small>
+        @enderror
     </div>
 
-    <div class="col-12">
-        <div class="position-relative pb-4">
-            <label for="input-tags" class="form-label text-primary">
-                Tags
+    <div class="col-12 position-relative pb-4">
+        <label for="input-tags" class="form-label text-primary">
+            Tags
+        </label>
+
+        <div class="dropdown">
+            <button class="form-control text-start" type="button" id="input-tags" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                None
+            </button>
+
+            <ul class="dropdown-menu dropdown-menu-start" aria-labelledby="input-tags">
+                @foreach ($tags as $tag)
+                    <li>
+                        <label class="dropdown-item" for="tag-{!! $tag->slug !!}">
+                            <input type="checkbox" name="tags[]" value="{!! $tag->id !!}"
+                                id="tag-{!! $tag->slug !!}" data-name="{!! $tag->name !!}"
+                                @checked(old('tag', $is_update_form ? $item_to_update->tags : [])->contains($tag->id))>
+                            {{ $tag->name }}
+                        </label>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+        @error('is_archived')
+            <small id="input-type-error" class="invalid-feedback position-absolute bottom-0 start-0">
+                {{ $message }}
+            </small>
+        @enderror
+    </div>
+
+    @if ($is_update_form)
+        <div class="col-12 position-relative pb-4">
+            <label for="input-type" class="form-label text-primary">
+                Status
             </label>
 
-            <div class="dropdown">
-                <button class="form-control text-start" type="button" id="input-tags" data-bs-toggle="dropdown"
-                    aria-expanded="false">
-                    None
-                </button>
+            <select class="form-control @error('is_archived') is-invalid @enderror" id="input-type" name="is_archived"
+                aria-errormessage="input-type-error" required>
+                <option value="0" @selected(!old('is_archived', $is_update_form ? boolval($item_to_update->is_archived) : false))>Not archived</option>
 
-                <ul class="dropdown-menu dropdown-menu-start" aria-labelledby="input-tags">
-                    @foreach ($tags as $tag)
-                        <li>
-                            <label class="dropdown-item" for="tag-{!! $tag->slug !!}">
-                                <input type="checkbox" name="tags[]" value="{!! $tag->id !!}"
-                                    id="tag-{!! $tag->slug !!}" data-name="{!! $tag->name !!}"
-                                    @checked(old('tag', $is_update_form ? $item_to_update->tags : [])->contains($tag->id))>
-                                {{ $tag->name }}
-                            </label>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
+                <option value="1" @selected(old('is_archived', $is_update_form ? boolval($item_to_update->is_archived) : false))>Archived</option>
+            </select>
             @error('is_archived')
                 <small id="input-type-error" class="invalid-feedback position-absolute bottom-0 start-0">
                     {{ $message }}
                 </small>
             @enderror
-        </div>
-    </div>
-
-    @if ($is_update_form)
-        <div class="col-12">
-            <div class="position-relative pb-4">
-                <label for="input-type" class="form-label text-primary">
-                    Status
-                </label>
-
-                <select class="form-control @error('is_archived') is-invalid @enderror" id="input-type"
-                    name="is_archived" aria-errormessage="input-type-error" required>
-                    <option value="0" @selected(!old('is_archived', $is_update_form ? boolval($item_to_update->is_archived) : false))>Not archived</option>
-
-                    <option value="1" @selected(old('is_archived', $is_update_form ? boolval($item_to_update->is_archived) : false))>Archived</option>
-                </select>
-                @error('is_archived')
-                    <small id="input-type-error" class="invalid-feedback position-absolute bottom-0 start-0">
-                        {{ $message }}
-                    </small>
-                @enderror
-            </div>
         </div>
     @endif
 
